d8b6bfcc7c28f11d6cd30b83840a2818b8210466
Yehonal <yehonal.azeroth@gmail.com>
2021-12-27 20:05:03 +0100

    sync(subrepo): changes from bash-lib

--- src/git-utils/subtree.sh
+++ src/git-utils/subtree.sh
@@ -1,12 +1,14 @@
 function subtreeFlow {
-    local name=$1
-    local path=$2
-    local repo=$3
-    local branch=$4
-    local prefix="$path/$name"
+    local repo=$1
+    local branch=$2
+    local prefix="$3"
 
     echo "> Adding subtree if not exists"
-    git subtree add   --prefix "$prefix" "$repo" "$branch"
-    echo "> Pulling latest changes from remote subtree"
-    git subtree pull  --prefix "$prefix" "$repo" "$branch"
+    git subtree add --prefix "$prefix" "$repo" "$branch"
+    echo "> Pulling latest changes from remote subtree: "$prefix" "$repo" "$branch""
+    git subtree pull --prefix "$prefix" "$repo" "$branch"  --squash
+    echo "> Push latest changes to remote subtree: "$prefix" "$repo" "$branch""
+    git subtree push --prefix "$prefix" "$repo" "$branch"  --squash
+
+    git subtree split --prefix "$prefix" -b "$branch"  --rejoin 
 }
