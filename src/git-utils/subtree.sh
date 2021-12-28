540eb5f8c3518e315d6ca507207c167c60326af4
Yehonal <yehonal.azeroth@gmail.com>
2021-12-28 12:02:10 +0100

    better messages

--- src/git-utils/subtree.sh
+++ src/git-utils/subtree.sh
@@ -12,12 +12,12 @@ function subtreeFlow {
     fi
 
     echo "> Pulling latest changes from remote subtree: "$prefix" "$repo" "$branch""
-    git subtree pull --prefix "$prefix" "$repo" "$branch" --squash
+    git subtree pull --prefix "$prefix" "$repo" "$branch" --squash  -m "sync(subtree): pull latest changes from $repo"
     echo "> Push latest changes to remote subtree: "$prefix" "$repo" "$branch""
     git subtree push --prefix "$prefix" "$repo" "$branch"
 
     echo "> Pulling again after a push: "$prefix" "$repo" "$branch""
-    git subtree pull --prefix "$prefix" "$repo" "$branch" --squash
+    git subtree pull --prefix "$prefix" "$repo" "$branch" --squash  -m "sync(subtree): sync pushed changes from $repo"
 
     #git subtree split --prefix "$prefix" -b "$branch"  --rejoin 
 }
