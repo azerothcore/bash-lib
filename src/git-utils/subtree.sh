35906f1abc8f73ac41a809100e1c8246822ddab1
Yehonal <yehonal.azeroth@gmail.com>
2022-02-20 14:30:54 +0100

    Upstream sync (#7)

--- src/git-utils/subtree.sh
+++ src/git-utils/subtree.sh
@@ -1,23 +1,57 @@
 function subtreeFlow {
     local repo=$1
-    local branch=$2
-    local prefix="$3"
+    local mainBranch=$2
+    local prBranch=$3
+    local prefix="$4"
+    local id="$5"
+
+    local curBranch=`git rev-parse --abbrev-ref HEAD`
+    local remoteId="temp-remote-$id"
+
+    echo "> Recreate branch"
+    git remote add "$remoteId" "$repo" || git remote set-url "$remoteId" "$repo" 
+    git push "$repo" --delete "$prBranch" || true
+    git fetch "$remoteId" "$mainBranch"
+    git push "$remoteId" "$remoteId/$mainBranch:refs/heads/$prBranch"
+
+    echo "> Switch to temporary branch"
+    git checkout -b "temp-update-subtree" "$curBranch"
 
     if [[ -d "$prefix" ]]; then
-        echo "> Split subtree for existing prefix $prefix $repo -b $branch..."
-        #git subtree split --prefix "$prefix" -b "$branch"
-    else
-        echo "> Adding subtree if not exists"
-        git subtree add --prefix "$prefix" "$repo" "$branch" --squash -m "sync(subtree): add $repo"
+        echo "> backup $prefix"
+        mv "$prefix" "$prefix-bak"
+
+        git commit -am "backup $prefix"
     fi
 
-    echo "> Pulling latest changes from remote subtree: "$prefix" "$repo" "$branch""
-    git subtree pull --prefix "$prefix" "$repo" "$branch" --squash  -m "sync(subtree): pull latest changes from $repo"
-    echo "> Push latest changes to remote subtree: "$prefix" "$repo" "$branch""
-    git subtree push --prefix "$prefix" "$repo" "$branch"
+    echo "> Adding subtree"
+    git subtree add --prefix "$prefix" "$remoteId" "$prBranch" --squash -m "sync(subtree): add $repo"
 
-    echo "> Pulling again after a push: "$prefix" "$repo" "$branch""
-    git subtree pull --prefix "$prefix" "$repo" "$branch" --squash  -m "sync(subtree): sync pushed changes from $repo"
+    echo "> Restore $prefix backup"
+    rm -rf $prefix
+    mv "$prefix-bak" "$prefix"
 
-    #git subtree split --prefix "$prefix" -b "$branch"  --rejoin 
+    git add -A
+    git diff-index --quiet HEAD || git commit -m "restore $prefix-bak"
+
+    echo "> Pulling latest changes from remote subtree: "$prefix" "$repo" "$prBranch""
+    git subtree pull --prefix "$prefix" "$remoteId" "$prBranch" --squash  -m "sync(subtree): pull latest changes from $repo"
+
+    echo "> Push latest changes to remote subtree: "$prefix" "$repo" "$prBranch""
+    git subtree push --prefix "$prefix" "$remoteId" "$prBranch"
+
+    echo "> Pulling again after a push: "$prefix" "$repo" "$prBranch""
+    git subtree pull --prefix "$prefix" "$remoteId" "$prBranch" --squash  -m "sync(subtree): sync pushed changes from $repo"
+
+    git add -A
+    git diff-index --quiet HEAD || git commit -m "commit all pulled changes"
+
+    echo "> Switch to previous branch and squash+delete temporary one"
+    git checkout "$curBranch"
+
+    git merge --squash "temp-update-subtree"
+
+    git branch -D "temp-update-subtree"
+
+    #git subtree split --prefix "$prefix" -b "$prBranch"  --rejoin 
 }
