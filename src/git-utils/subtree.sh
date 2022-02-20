238284b40618cc2e7027867b1ec7e17362208b38
Yehonal <giuseppe@bitvavo.com>
2022-02-20 13:12:41 +0100

    Upstream sync (#3)

--- src/git-utils/subtree.sh
+++ src/git-utils/subtree.sh
@@ -3,21 +3,46 @@ function subtreeFlow {
     local branch=$2
     local prefix="$3"
 
+    local curBranch=`git rev-parse --abbrev-ref HEAD`
+
+    echo "> Switch to temporary branch"
+    git checkout -b "temp-update-subtree" "$curBranch"
+
     if [[ -d "$prefix" ]]; then
-        echo "> Split subtree for existing prefix $prefix $repo -b $branch..."
-        #git subtree split --prefix "$prefix" -b "$branch"
-    else
-        echo "> Adding subtree if not exists"
-        git subtree add --prefix "$prefix" "$repo" "$branch" --squash -m "sync(subtree): add $repo"
+        echo "> backup $prefix"
+        mv "$prefix" "$prefix-bak"
+
+        git commit -am "backup $prefix"
     fi
+
+    echo "> Adding subtree"
+    git subtree add --prefix "$prefix" "$repo" "$branch" --squash -m "sync(subtree): add $repo"
+
+    echo "> Restore $prefix backup"
+    rm -rf $prefix
+    mv "$prefix-bak" "$prefix"
+
+    git add -A
+    git diff-index --quiet HEAD || git commit -m "restore $prefix-bak"
 
     echo "> Pulling latest changes from remote subtree: "$prefix" "$repo" "$branch""
     git subtree pull --prefix "$prefix" "$repo" "$branch" --squash  -m "sync(subtree): pull latest changes from $repo"
+
     echo "> Push latest changes to remote subtree: "$prefix" "$repo" "$branch""
     git subtree push --prefix "$prefix" "$repo" "$branch"
 
     echo "> Pulling again after a push: "$prefix" "$repo" "$branch""
     git subtree pull --prefix "$prefix" "$repo" "$branch" --squash  -m "sync(subtree): sync pushed changes from $repo"
+
+    git add -A
+    git diff-index --quiet HEAD || git commit -m "commit all pulled changes"
+
+    echo "> Switch to previous branch and squash+delete temporary one"
+    git checkout "$curBranch"
+
+    git merge --squash "temp-update-subtree"
+
+    git branch -D "temp-update-subtree"
 
     #git subtree split --prefix "$prefix" -b "$branch"  --rejoin 
 }
